4474df12c3396266f720f8b42fa828d1d4cc8cf0
Robinson U. <robinson.urena@hytechhome.com>
2024-10-23 15:50:09 -0400

    FEAT: Authenticacion done

---
+++ app/Http/Requests/SignUpRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignUpRequest extends FormRequest {
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool {
+
+        return true;
+
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array {
+
+        return [
+            'name' => ['required', 'string', 'min:8', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:Users'],
+            'password' => ['required', 'string', 'min:8', 'max:32']
+        ];
+
+    }
+
+    public function passedValidation() {
+
+        ['password' => $password] = $this->input();
+
+        $this->merge(['password' => bcrypt($password)]);
+
+    }
+
+}
